34dcc3c35b93c7459df854fb5c96d14246696ebb
MohammedQubaisuddin <qubaisuddin.mohammed@gytworkz.com>
2022-05-12 00:08:08 +0530

    commit 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { Container } from "react-bootstrap";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
     <>
-      <h1>welcome to shopHere</h1>
+    <Router>
+      <Header></Header>
+      <main className="py-3">
+        {/* container makes the element in the middle */}
+        <Container>
+        <Routes>
+          <Route path='/'  element={<HomeScreen></HomeScreen>}></Route>
+          <Route path='/product/:id' element={<ProductScreen></ProductScreen>}></Route>
+        </Routes>
+        </Container>
+      </main>
+      <Footer></Footer>
+      </Router>
     </>
   );
 };
